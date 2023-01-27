953c35b3914127f96a951caa8c99eac49269f311
gargparthb <62770333+gargparthb@users.noreply.github.com>
2023-01-26 23:31:59 -0500

    drawing stuff

--- cubie.py
+++ cubie.py
@@ -1,13 +1,37 @@
 import numpy as np
 
+
 class Cubie:
     def __init__(self, current_position, solved_position, normals):
         self.current_position = current_position
         self.solved_position = solved_position
+        # array of white and green normals
         self.normals = normals
 
     def __str__(self):
         return f"Cubie from {self.solved_position} at {self.current_position} with normals: {self.normals}"
+
+    def get_colors(self, shades):
+        colors = {
+                  tuple(self.normals[0]): shades["white"],
+                  tuple(self.normals[0].__mul__(-1)): shades["yellow"],
+                  tuple(self.normals[1]): shades["green"],
+                  tuple(self.normals[1].__mul__(-1)): shades["blue"]}
+        normals_cross = np.cross(self.normals[0],
+                                 self.normals[1])
+        colors[tuple(normals_cross)] = shades["red"]
+        colors[tuple(normals_cross.__mul__(-1))] = shades["orange"]
+        return colors
+
+    def x(self):
+        return self.current_position[0]
+
+    def y(self):
+        return self.current_position[1]
+
+    def z(self):
+        return self.current_position[2]
+
 
 def make_cube():
     cube = []
--- draw.py
+++ draw.py
@@ -1,23 +1,59 @@
 import pyglet
 from pyglet import shapes
+import numpy as np
+import cubie
 
-window = pyglet.window.Window()
+color_scheme = {
+    "white": (255, 255, 255),
+    "green": (0, 155, 72),
+    "red": (183, 18, 52),
+    "blue": (0, 70, 173),
+    "orange": (255, 88, 0),
+    "yellow": (255, 213, 0)
+}
+
+black = (0, 0, 0)
+
+size = 40
+border = 5
+
+
+def scale(n):
+    return size * n
+
+
+window = pyglet.window.Window(size * 12, size * 9)
 batch = pyglet.graphics.Batch()
 
-rectangle = shapes.Rectangle(250, 300, 50, 50, color=(255, 255, 255), batch=batch)
+stickers = [0 for x in range(56)]
 
 
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width // 2, y=window.height // 2,
-                          anchor_x='center', anchor_y='center')
+def draw_cube(cube):
+    draw_face(cube, lambda c: c.y() == 1, lambda qb: (qb.x() + 4, qb.z() + 7), (0, 1, 0), 0)
+    draw_face(cube, lambda c: c.y() == -1, lambda qb: (qb.x() + 4, qb.z() + 1), (0, -1, 0), 9)
+    draw_face(cube, lambda c: c.x() == -1, lambda qb: (qb.y() + 1, qb.z() + 4), (-1, 0, 0), 18)
+    draw_face(cube, lambda c: c.x() == 1, lambda qb: (qb.y() + 7, qb.z() + 4), (1, 0, 0), 27)
+    draw_face(cube, lambda c: c.z() == 1, lambda qb: (qb.x() + 4, qb.y() + 4), (0, 0, 1), 36)
+    draw_face(cube, lambda c: c.z() == -1, lambda qb: (qb.x() + 10, qb.y() + 4), (0, 0, -1), 45)
+
+
+def draw_face(cube, cond, pos_func, norm, index):
+    for qb in filter(cond, cube):
+        draw_tile(pos_func(qb), qb.get_colors(color_scheme)[norm], index)
+        index += 1
+
+
+def draw_tile(pos, color, index):
+    stickers[index] = shapes.BorderedRectangle(scale(pos[0]), scale(pos[1]), size, size,
+                                               border=border, color=color, border_color=black, batch=batch)
+
+
+draw_cube(cubie.make_cube())
 
 
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
     batch.draw()
 
 
